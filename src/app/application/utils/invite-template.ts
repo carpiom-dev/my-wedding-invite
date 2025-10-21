7859e4efa493baed85a95a5ecffa87303256c929
Manuel Carpio <manuel.carpio@guru-soft.com>
2025-10-21 11:48:27 -0500

    � docs: Actualizar README con estructura de proyecto

--- src/app/application/utils/invite-template.ts
+++ src/app/application/utils/invite-template.ts
@@ -2,10 +2,13 @@ import { Person } from '@domain/entities/person';
 
 export function buildInviteText(person: Person): string {
   const nombre = [person.nombre, person.apellido].filter(Boolean).join(' ');
-  const baseUrl = 'https://tuboda.com/invitacion';
-  const url = `${baseUrl}?nombre=${encodeURIComponent(nombre)}&id=${encodeURIComponent(person.id.value)}`;
+  const baseUrl = 'https://carpiom-dev.github.io/wedding-invite';
 
-  return `💍 *Querido(a) ${nombre}*,
+  const invitados = 3;
+
+  const url = `${baseUrl}?nombre=${encodeURIComponent(nombre)}&invitados=${encodeURIComponent(invitados)}`;
+
+    return `💍 *Querido(a) ${nombre}*,
 
 Con todo nuestro cariño, queremos invitarte a compartir un día muy especial en nuestras vidas.
 Tu presencia hará de este momento algo inolvidable ✨
